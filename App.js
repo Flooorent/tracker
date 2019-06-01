7bfd21d21d1d6dae979a8c8cc3f94af1a90baa9d
Flooorent <florent.moiny@gmail.com>
2019-06-01 12:27:42 +0200

    feature: add calendars and pop-ups

--- App.js
+++ App.js
@@ -1,11 +1,71 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {Calendar} from 'react-native-calendars'
+import Dialog, {DialogButton, DialogContent, DialogTitle} from 'react-native-popup-dialog'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      markedDates: {
+        '2019-06-01': {selected: true, selectedColor: 'blue'},
+        '2019-06-02': {selected: true, selectedColor: 'blue'},
+        '2019-06-03': {selected: true, selectedColor: 'blue'},
+        '2019-06-04': {selected: true, selectedColor: 'red'},
+        '2019-06-05': {selected: true, selectedColor: 'blue'},
+      },
+      displayDialog: true,
+      dialogTitle: '',
+    }
+  }
+
+  showDialog(day) {
+    this.setState({
+      displayDialog: true,
+      dialogTitle: day.day,
+    })
+  }
+
+  clearDialog() {
+    this.setState({
+      displayDialog: false,
+      dialogTitle: ''
+    })
+  }
+
   render() {
+    const today = new Date()
+    const minDate = '1998-17-12'
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+
+        <Calendar
+          maxDate={today}
+          minDate={minDate}
+          onDayPress={(day) => this.showDialog(day)}
+          hideExtraDays={false}
+          markedDates={this.state.markedDates}
+        />
+
+        <Dialog
+          visible={this.state.displayDialog}
+          onTouchOutside={() => this.clearDialog()}
+          dialogTitle={<DialogTitle title={this.state.dialogTitle}/>}
+        >
+          {/* TODO: use flexDirection: row; for buttons */}
+          <DialogContent>
+            <DialogButton
+              text="Win"
+              onPress={() => console.log('Pressed WIN')}
+            />
+            <DialogButton
+              text="Loss"
+              onPress={() => console.log('Pressed LOSS')}
+            />
+          </DialogContent>
+        </Dialog>
+
       </View>
     );
   }
@@ -17,5 +77,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  dialog: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
 });
